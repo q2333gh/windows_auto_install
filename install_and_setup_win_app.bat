f3983c3317b767c86b1655068627eaf220da03b9
btwl <1253102853@qq.com>
2023-08-12 10:40:42 +0800

    init

--- install_and_setup_win_app.bat
+++ install_and_setup_win_app.bat
@@ -1,16 +1,21 @@
-choco download script
-# todo : how to specify use choco to install  software location ?
-how to all eng edition:
-----base:
+rem rem在.bat文件表示注释.好笨的syntax
+rem prereq: choco .目的是命令行一键静默安装软件.另外choco还能一键全部升级
+rem choco的功能和360软件管家有点像,不过360还不能静默安装.
+rem todo. windows下的软件带GUI的安装程序能否全部用CLI接管.毕竟就几个关键按钮做输入输出.
+
+rem # todo : how to specify use choco to install  software location ?
+rem ----base:
 sublime text
-windows terminal , with color settings
+windows terminal
+rem with color settings. already default install in win11. just some configs . set ubuntu as default .
+rem and set windows terminal as default terminal 
 everything 
-utools
+utools 
 honeyview
 sumatrapdf
 free downlaod manager
 potplayer
-VPN: v2rayn with remote addr
+VPN: v2rayn with remote addr 
 >>>office. maybe need iso file 
 wiztree
 draw.io
@@ -18,7 +23,7 @@ xmind
 360zip. but without pop up ads.比如提醒装360或者升级.有点不适. 还没找到方便的平替品.右键菜单较为方便.GUI也不错
 QQMusic,除了要收钱都好.当播放器用过
 EarTrumpt ,不知道win11是否需要这个方便的gui调节
--------optionnal:
+rem -------optionnal:
 xunlei  -> 有一些下载不动的东西他是真的下得动.但是最好咋个让他在沙箱里头运行安静一些,别对电脑偷偷摸摸
 huorong : 主要用来对进程的网络拦截和弹窗关闭
 
@@ -38,7 +43,7 @@ hardware info  or LibreHardwareMonitor-net472  or AIDA64
 
 
 
----------commnunication:
+rem ---------commnunication:
 TIM  特别是QProtect这个进程
 wechat 这两个软件不知道会不会窃取本机隐私,不知道有什么全局daemon软件可以监控所有网络和文件读写行为.
 
@@ -46,7 +51,7 @@ wechat 这两个软件不知道会不会窃取本机隐私,不知道有什么全
 
 
 
------dev:
+rem -----dev:
 
 vscode , with settings sync remote
 python ,maybe with PATH (win)
@@ -61,13 +66,13 @@ VS
 XFTP
 jupyter-notebook
 ...
---game
+rem --game
 
 steam
 steam++
 
 
---device specific: Y9000p 2022
+rem --device specific: Y9000p 2022
 legion zone
 爱石头键盘驱动
 罗技 onboard-mem-manager
